7ab9d210ff0433c2b35b9bbc3cd179582cc5d1f0
Joel Plowright <joelyjoel@protonmail.com>
2021-03-31 14:36:08 +0100

    parse accidentals

--- src/features/sequencer/parsePitch.ts
+++ src/features/sequencer/parsePitch.ts
@@ -21,17 +21,26 @@ export function parsePitch(inputStr:string): PitchParse {
 
     // Get letter
     let letterResult = str[0]
+    let accidentalResult = str[1];
     if(letterResult) { 
       let letter = letterResult[0].toUpperCase();
       let pitchClass = letterValues[letter];
+      let midiNumber = pitchClass + 49
+      if(accidentalResult == '#')
+        midiNumber++;
+      else if(accidentalResult == 'b')
+        midiNumber--;
+      else if(accidentalResult)
+        throw `"${str}" is not a musical note`
+      
       if(pitchClass !== undefined)
         return {
           hasPitch: true,
-          midiNumber: pitchClass + 49,
+          midiNumber,
           str,
         }
       else
-        throw `"${letter}" is not a musical note`;
+        throw `"${str}" is not a musical note`;
     } else
       throw "Could not find letter";
   } catch(err) {
